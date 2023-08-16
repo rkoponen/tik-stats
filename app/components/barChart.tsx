42aeba4c34dda87cf2ee5e61fefcd395cef689a8
rkoponen <ruupert.koponen@gmail.com>
2023-08-16 13:02:07 +0300

    Set title for chart

--- app/components/barChart.tsx
+++ app/components/barChart.tsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import 'chart.js/auto';
 import {Bar} from "react-chartjs-2";
 import { ChartData, ChartOptions } from "chart.js/auto";
+import { Months } from "../page";
 
 export interface BarChartProps {
   data: ChartData<"bar">;
   maxVal: number;
+  month: number
 }
 
-const BarChart: React.FC<BarChartProps> = ({ data, maxVal }) => {
+const BarChart: React.FC<BarChartProps> = ({ data, maxVal, month }) => {
 
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
@@ -25,17 +27,32 @@ const BarChart: React.FC<BarChartProps> = ({ data, maxVal }) => {
   }, [])
   const indexAxis = window.innerWidth > 768 ? 'x' : 'y'
 
-  let scales = windowSize.width > 768 ? { y: { suggestedMax: maxVal }} : { x: { suggestedMax: maxVal } }
+  let scales = windowSize.width > 768 ? 
+    { y: { suggestedMax: maxVal, grid: { display: false } }} 
+    : { x: { suggestedMax: maxVal, grid: {display: false} } }
 
   const options: ChartOptions<"bar"> = {
     indexAxis: indexAxis,
     responsive: true,
     maintainAspectRatio: false,
-    scales: scales
+    scales: scales,
+    plugins: {
+      title: {
+        display: true,
+        text: `Daily view count (${Months[month]})`,
+        font: {
+          size: 20,
+          weight: 'bold',
+        }
+      },
+      legend: {
+        display: false,
+      }
+    }
   };
 
   return (
-    <div className="w-full h-300">
+    <div className="w-full h-300 bg-stone-50 rounded-lg p-5">
       <Bar style={ {width: '100%', height: '500px'} } data={data} options={options}></Bar>
     </div>
   )
