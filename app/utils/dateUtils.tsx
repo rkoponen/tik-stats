37ffd802c30195ac0e02828b6424c274a41c1473
rkoponen <ruupert.koponen@gmail.com>
2023-09-02 16:43:51 +0300

    Remove null return

--- app/utils/dateUtils.tsx
+++ app/utils/dateUtils.tsx
@@ -1,8 +1,8 @@
 import { DateCount, DateCountArray } from "../types/dateCountArray";
 import { VideoBrowsingHistoryItem } from "../types/jsonInterfaces";
 
-export const computeDailyCounts = (videoHistory: VideoBrowsingHistoryItem[]): DateCountArray | null => {
-  if (videoHistory) {
+export const computeDailyCounts = (videoHistory: VideoBrowsingHistoryItem[]): DateCountArray => {
+
     const dateCountMap: Map<string, DateCount> = new Map();
 
   videoHistory.forEach(video => {
@@ -15,21 +15,17 @@ export const computeDailyCounts = (videoHistory: VideoBrowsingHistoryItem[]): Da
     } else {
       dateCountMap.set(formattedDateKey, { date, count: 1 });
     }
-});
+  });
 
-    const dateCountPairs = Array.from(dateCountMap.values());
-    return dateCountPairs;
-  } else {
-    return null;
-  }
+  const dateCountPairs = Array.from(dateCountMap.values());
+  return dateCountPairs;
 }
 
 export const getMonthlyData = (data: DateCountArray, month: number): DateCountArray => {
-  console.log(`Month from monthlyData: ${month}`)
   return data.filter(pair => pair.date.getMonth() === month)
 }
 
-export const getLatestMonth = (data: DateCountArray): number | null => {
+export const getLatestMonth = (data: DateCountArray): number => {
   const months = data.map(pair => pair.date.getMonth())
   return Math.max(...months);
 }
